20ea82de672dd1b9ee863e4cbd649d67b29be16d
Erick Martins Ratamero <erickmartins@gmx.net>
2021-08-08 21:56:19 -0400

    running globus if not already running

--- example_run.bat
+++ example_run.bat
@@ -1,2 +1,3 @@
+TASKLIST | FINDSTR globus_connect || START "" "Path\globus_connect_personal.exe"
 call activate globus
 python PATH_TO_python_subprocess_test.py --svcuser SVC-USER LOCALENDPOINT --localdir /~/C/PATH_TO_LOCALDIR --remotedir PATH_TO_REMOTEDIR
